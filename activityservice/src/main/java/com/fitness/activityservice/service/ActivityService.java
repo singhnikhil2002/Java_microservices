6325eca8f7b6fe1322d4e422b58d4633ae2e1c74
Nikhil Singh <singhnikhil19832011@gmail.com>
2025-11-15 00:28:39 +0530

    Commits for Activity Service

---
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,20 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.ActivityRepository;
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.model.Activity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+//    public ActivityResponse trackActivity(ActivityRequest request) {
+//        Activity activity = Activity.builder()
+//                .userId(r)
+//    }
+}
